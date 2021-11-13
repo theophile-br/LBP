25cbd9310da21101ccf9e5c5f271a79ef4087c18
theophile.brayé <theophile.braye@uha.fr>
2021-11-13 13:48:02 +0100

    Add one more graph

--- python-scripts/test.py
+++ python-scripts/test.py
@@ -8,13 +8,12 @@ import csv
 width = 0.35
 for subdir, dirs, files in os.walk("../dataset/PROCESS/graph/"):
     for file in files:
-        if "_confusion_matrice.csv" in file:
+        if file.endswith("_confusion_matrice.csv"):
             csv_data = []
             with open('../dataset/PROCESS/graph/' + file) as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
                 for row in csv_reader:
                     csv_data.append(row)
-
             y = np.asarray(csv_data)[:1, 1:][0]  # label
             label = csv_data[0][0].split("/")  # Expert/Classifier
             data = np.asarray(csv_data)[1:, 1:].astype(int)  # data
@@ -26,3 +25,25 @@ for subdir, dirs, files in os.walk("../dataset/PROCESS/graph/"):
             c1.set(ylabel="True label", xlabel="Predicted label")
             # plt.show()
             plt.savefig('../dataset/PROCESS/graph/' + file + '.jpg')
+
+csv_data = []
+with open('../dataset/PROCESS/graph/distance_success_rate.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        csv_data.append(row)
+
+x = np.asarray(csv_data)[:1].flatten()  # label
+data = np.asarray(csv_data)[1:].flatten().astype(float)  # data
+fig, ax = plt.subplots()
+for i in range(0, len(x)):
+    ax.bar(x[i], data[i], width)
+
+plt.title("Distance success rate")
+plt.xlabel("Distance formula")
+plt.ylabel("Pourcent (%)")
+plt.ylim([0, 100])
+
+for index, value in enumerate(data):
+    plt.text(index - 0.15, value + 0.5, str(value) + "%")
+
+plt.savefig('../dataset/PROCESS/graph/distance_success_rate.jpg')
