74935c3ddcf96833aa7b5b67f740d1969185dd48
theophile.brayé <theophile.braye@uha.fr>
2021-11-14 11:39:31 +0100

    fix python script

--- python-scripts/test.py
+++ python-scripts/test.py
@@ -6,39 +6,39 @@ import os
 import csv
 
 program_path = os.path.dirname(os.path.abspath(__file__))
-
+program_path = os.path.join(program_path, "..")
 width = 0.35
-for subdir, dirs, files in os.walk(os.path.join(program_path, "..", "PROCESS", "graph")):
+for subdir, dirs, files in os.walk(os.path.join(program_path, "PROCESS", "graph")):
     for file in files:
         if file.endswith("_confusion_matrice.csv"):
             csv_data = []
-            with open(os.path.join(program_path, "..", "PROCESS", "graph", file)) as csv_file:
+            with open(os.path.join(program_path, "PROCESS", "graph", file)) as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                csv_data.append(row)
-            y = np.asarray(csv_data)[:1, 1:][0]  # label
-            label = csv_data[0][0].split("/")  # Expert/Classifier
-            data = np.asarray(csv_data)[1:, 1:].astype(int)  # data
-            df_cm = pd.DataFrame(data, index=[i for i in y],
-                                 columns=[i for i in y])
-            plt.figure(figsize=(10, 7))
-            plt.title("Matrice de confusion " + file.split("_")[0])
-            c1 = sn.heatmap(df_cm, annot=True, fmt="1")
-            c1.set(ylabel="True label", xlabel="Predicted label")
-            # plt.show()
-            plt.savefig('../dataset/PROCESS/graph/' + file + '.jpg')
+                for row in csv_reader:
+                    csv_data.append(row)
+                y = np.asarray(csv_data)[:1, 1:][0]  # label
+                label = csv_data[0][0].split("/")  # Expert/Classifier
+                data = np.asarray(csv_data)[1:, 1:].astype(int)  # data
+                df_cm = pd.DataFrame(data, index=[i for i in y],
+                                     columns=[i for i in y])
+                plt.figure(figsize=(10, 7))
+                plt.title("Matrice de confusion " + file.split("_")[0])
+                c1 = sn.heatmap(df_cm, annot=True, fmt="1")
+                c1.set(ylabel="True label", xlabel="Predicted label")
+                # plt.show()
+                plt.savefig(os.path.join(program_path, 'PROCESS', 'graph', file + '.jpg'))
 
             csv_data = []
-            with open('../dataset/PROCESS/graph/distance_success_rate.csv') as csv_file:
+            with open(os.path.join(program_path, 'PROCESS', 'graph', "distance_success_rate.csv")) as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                csv_data.append(row)
+                for row in csv_reader:
+                    csv_data.append(row)
 
-            x = np.asarray(csv_data)[:1].flatten()  # label
-            data = np.asarray(csv_data)[1:].flatten().astype(float)  # data
-            fig, ax = plt.subplots()
-            for i in range(0, len(x)):
-                ax.bar(x[i], data[i], width)
+                x = np.asarray(csv_data)[:1].flatten()  # label
+                data = np.asarray(csv_data)[1:].flatten().astype(float)  # data
+                fig, ax = plt.subplots()
+                for i in range(0, len(x)):
+                    ax.bar(x[i], data[i], width)
 
             plt.title("Distance success rate")
             plt.xlabel("Distance formula")
@@ -48,4 +48,4 @@ for subdir, dirs, files in os.walk(os.path.join(program_path, "..", "PROCESS", "
             for index, value in enumerate(data):
                 plt.text(index - 0.15, value + 0.5, str(value) + "%")
 
-            plt.savefig(os.path.join(program_path, '..', 'PROCESS', 'graph', 'distance_success_rate.jpg'))
+            plt.savefig(os.path.join(program_path, 'PROCESS', 'graph', 'distance_success_rate.jpg'))
